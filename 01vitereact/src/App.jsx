bfb4b5b842f54652c7c284b644cb554ba0754c67
bhutuklearning <amritanshugoutam.dev@gmail.com>
2024-01-08 15:25:22 +0530

    Learning is advancing

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,9 +2,11 @@ import Chai from './chai.jsx';
 
 function App() {
 
+  const username = "Chai aur code";
+
   return (
     <>
-    <h1>Hello World from Amritanshu Goutam. || Creating with Vite</h1>
+    <h1>Hello World from Amritanshu Goutam. || Creating with Vite || {username}</h1>
     <Chai/>
     <p>Learning is cool and having patience to continue it is the key.</p>
     </>
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom Function in Main.jsx</h1>
+    </div>
+  )
+}
+
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>Visit Google!</a>
+);
+
+const anotherusername = "React+Vite"
+
+const reactElement = React.createElement(
+  'a', {href: "https://google.com", target: "_blank"},
+  'Click Me to visit Google.', 
+  anotherusername
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <React.StrictMode>
+  <App />
+</React.StrictMode>
 )
+
+
+// anotherElement
+//  reactElement
