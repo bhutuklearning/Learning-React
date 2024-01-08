c9df456b053f1d27df838db67485640680cf9edb
bhutuklearning <amritanshugoutam.dev@gmail.com>
2024-01-08 19:47:33 +0530

    Props and Tailwind Css

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    name:  "Bhutuk",
+    age: 21,
+  }
+
+  let newArr = [1, 2, 3, 4];
+
+  return (
+    <>
+      <h1 className='bg-green-500 text-black p-4 rounded-xl mb-4'>Hello Tailwind CSS | Tailwind Test</h1>
+      <Card username = "bhutuklearning" btnText = "Shoot the Button"/> 
+      <Card  username = "Amritanshu Goutam" btnText = "Click for Profile"/>
+      <Card  username = "Jigyasu Learning" />
+    </>
+  )
+}
+
+export default App
+
+//someObj = {myObj}
+//someObj={newArr}
